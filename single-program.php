7cdc39f62ba96d545206ccebd00a5bc26144ff57
Stephanie Ortiz <steph@imaginatii.com>
2019-06-12 18:09:28 -0400

    Visually aligning programs template

--- single-program.php
+++ single-program.php
@@ -9,8 +9,15 @@
 
 get_header();
 ?>
-
+ 
 	<div id="primary" class="content-area">
+             <header class="entry-header" <?php if (has_post_thumbnail( $post->ID ) ): ?>
+          
+          <?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); ?>
+          style="background-image: url('<?php echo $image[0]; ?>');"  id="custom-bg" <?php endif; ?> >
+          <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+     
+  </header>
 		<main id="main" class="site-main">
 
 		<?php
